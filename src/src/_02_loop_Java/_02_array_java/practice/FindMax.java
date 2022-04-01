4ce7ceed7db84c843d2df7f7a0b8d21c3933e504
ManhcuongG1 <tranmanhcuongqbdl1@gmail.com>
2022-04-01 19:00:40 +0700

    up

--- src/src/_02_loop_Java/_02_array_java/practice/FindMax.java
+++ src/src/_02_loop_Java/_02_array_java/practice/FindMax.java
@@ -1,17 +1,5 @@
 package _02_loop_Java._02_array_java.practice;
 
 public class FindMax {
-    public static void main(String[] args) {
-        int[] arr = {4, 12, 7, 8, 1, 6, 9};
-        int index = minValue(arr);
-        System.out.println("The smallest element in the array is: " + arr[index]);
-        public static int minValue(int[] array) {
-            int index = 0;
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[index]) {
-                    index = i;
-                }
-            }
-            return index;
-    }
+
 }
--- src/src/_02_loop_Java/exercise_loop/DisplayPrime.java
+++ src/src/_02_loop_Java/exercise_loop/DisplayPrime.java
@@ -1,48 +1,53 @@
 package _02_loop_Java.exercise_loop;
 
+import java.util.Scanner;
+
 public class DisplayPrime {
     public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        int numbers;
-//        int count = 0;
-//        int n = 2;
-//        System.out.println("Input numbers: ");
-//        numbers = input.nextInt();
-//        while (count <numbers) {
-//            int k = 0;
-//            for (int i = 2; i < n; i++) {
-//                if (n % i == 0) {
-//                    k++;
-//                }
-//                }
-//            if (k == 0) {
-//                System.out.print(n+" ");
-//                count++;
-//            }
-//            n++;
-//        }
-//
-//    }
-        int number = 0;
+        int numbers = 20;
         int count = 0;
-        while (count < 20) {
-            boolean check = true;
-            if (number < 2) {
-                check = false;
-            } else {
-                for (int i = 2; i < number; i++) {
-                    if (number % i == 0) {
-                        check = false;
-                        break;
-                    }
-                }
-                if (check) {
-                    System.out.println(number + " is the a prime number");
-                    count++;
+        int n = 2;
+        while (count < numbers) {
+            int k = 0;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    k++;
                 }
             }
-            number++;
+            if (k == 0) {
+                System.out.println(n + " is the a prime number");
+                count++;
+            }
+            n++;
         }
 
     }
 }
+
+
+// Bài tham khảo của a Duy:
+
+
+//        int number = 0;
+//        int count = 0;
+//        while (count < 20) {
+//            boolean check = true;
+//            if (number < 2) {
+//                check = false;
+//            } else {
+//                for (int i = 2; i < number; i++) {
+//                    if (number % i == 0) {
+//                        check = false;
+//                        break;
+//                    }
+//                }
+//                if (check) {
+//                    System.out.println(number + " is the a prime number");
+//                    count++;
+//                }
+//            }
+//            number++;
+//        }
+
+//    }
+//}
